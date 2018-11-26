98e542927eff808eba315690c650409d563ff470
Thorvald Ballestad <thorvald.tb@gmail.com>
2018-11-26 16:33:47 +0100

    Adds wrapper in index.html to center content

--- index.php
+++ index.php
@@ -1,15 +1,25 @@
 <html>
     <head>
+	<style>
+	 .wrapper {
+	     width: 500px;
+	     margin: 0 auto;
+	     text-align: center;
+	 }
+	</style>
     </head>
     <body>
-      <?php
-	$msg = $_GET['msg'];
-	if ($msg) {
-	echo "<h2>" . $msg . "</h2>";
-	}
-	?>
-	<form action="register.php" >
-	    <input type="Submit" value="Jeg drikker kaffe!">
-	</form>
+	<div class="wrapper" >
+	    <?php
+	    $msg = $_GET['msg'];
+	    if ($msg) {
+		echo "<h2>" . $msg . "</h2>";
+	    }
+	    ?>
+	    
+	    <form action="register.php" >
+		<input type="Submit" value="Jeg drikker kaffe!">
+	    </form>
+	</div>
     </body>
 </html>
